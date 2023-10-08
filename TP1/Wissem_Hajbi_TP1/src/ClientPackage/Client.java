1335c265a0a9ee4f1c9136ab41a5ec87d3b42ae0
WissemHajbi <wissemhajbi2002@gmail.com>
2023-10-08 20:03:49 +0100

    fixes

--- TP1/Wissem_Hajbi_TP1/src/ClientPackage/Client.java
+++ TP1/Wissem_Hajbi_TP1/src/ClientPackage/Client.java
@@ -3,6 +3,8 @@ package ClientPackage;
 import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 
 public class Client {
     public static void main(String[] args){
@@ -10,6 +12,12 @@ public class Client {
             InputStream input = socket.getInputStream();
             OutputStream output = socket.getOutputStream();)
         {
+
+            InetAddress IA = InetAddress.getByName("10.27.15.178");
+		    InetSocketAddress ISA = new InetSocketAddress(IA,1234);
+		    Socket client = new Socket();
+		    client.connect(ISA);
+
             System.out.println("connected");
             Scanner scanner = new Scanner(System.in);
             System.out.print("Please enter an integer: ");
--- TP1/Wissem_Hajbi_TP1/src/ServerPackage/Server.java
+++ TP1/Wissem_Hajbi_TP1/src/ServerPackage/Server.java
@@ -3,8 +3,7 @@ package ServerPackage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
+
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -15,17 +14,9 @@ public class Server {
             Socket clientSocket = ss.accept();
             InputStream input = clientSocket.getInputStream();
             OutputStream output = clientSocket.getOutputStream();
-
-            InetAddress IA = InetAddress.getByName("10.27.15.178");
-		    InetSocketAddress ISA = new InetSocketAddress(IA,1234);
-		    Socket client = new Socket();
-		    client.connect(ISA);
-
-            System.out.println("1");
-
+            
             int in = input.read();
 
-            System.out.println("2");
             in *= 5;
 
             System.out.println(in);
