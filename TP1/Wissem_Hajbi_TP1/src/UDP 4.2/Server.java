51be8b0b58ab8d09775ea316a39e50c90733e9a2
WissemHajbi <wissemhajbi2002@gmail.com>
2023-11-07 22:30:40 +0100

    4.2

---
+++ TP1/Wissem_Hajbi_TP1/src/UDP 4.2/Server.java
@@ -0,0 +1,46 @@
+package act4_2;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Serveur {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			    int serverPort = 1236;
+	            DatagramSocket serverSocket = new DatagramSocket(serverPort);
+	            System.out.println("Le serveur est en attente de connexions..."); 
+	            while (true) { 
+	            	
+	                byte[] receiveHeure = new byte[1024];
+	                // Crée un tableau de bytes pour stocker les données reçues
+	                DatagramPacket receivePacketHeure = new DatagramPacket(receiveHeure, receiveHeure.length); 
+	                // Crée un paquet pour recevoir des données
+	                serverSocket.receive(receivePacketHeure); 
+	                // Attend la réception de données depuis un client
+	                String clientMessageHeure = new String(receivePacketHeure.getData(), 0, receivePacketHeure.getLength());
+	                System.out.println(clientMessageHeure);
+	                String heureResponse ="Heure:  "+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+	                byte[] sendHeure= heureResponse.getBytes(); 
+	                // Convertit le message de réponse en tableau de bytes
+	                InetAddress clientAddress = receivePacketHeure.getAddress();
+	                // Obtient l'adresse IP du client
+	                int clientPort = receivePacketHeure.getPort();
+	                DatagramPacket sendPacket1 = new DatagramPacket(sendHeure, sendHeure.length, clientAddress, clientPort);
+	                // Crée un paquet pour envoyer la réponse au client
+	                serverSocket.send(sendPacket1); 
+	                // Envoie la réponse au client
+
+		}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	 }
+
+}
--- TP1/Wissem_Hajbi_TP1/src/UDP/Server.java
+++ TP1/Wissem_Hajbi_TP1/src/UDP/Server.java
@@ -1,46 +1,52 @@
-package act4_2;
-
+package act4_1;
+import java.net.DatagramSocket;
 import java.io.IOException;
 import java.net.DatagramPacket;
-import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Serveur {
-
-	public static void main(String[] args) {
+public class Server {
+	
+	public static void main(String[] args)  {
 		// TODO Auto-generated method stub
+		
 		try {
-			    int serverPort = 1236;
-	            DatagramSocket serverSocket = new DatagramSocket(serverPort);
-	            System.out.println("Le serveur est en attente de connexions..."); 
-	            while (true) { 
-	            	
-	                byte[] receiveHeure = new byte[1024];
-	                // Crée un tableau de bytes pour stocker les données reçues
-	                DatagramPacket receivePacketHeure = new DatagramPacket(receiveHeure, receiveHeure.length); 
-	                // Crée un paquet pour recevoir des données
-	                serverSocket.receive(receivePacketHeure); 
-	                // Attend la réception de données depuis un client
-	                String clientMessageHeure = new String(receivePacketHeure.getData(), 0, receivePacketHeure.getLength());
-	                System.out.println(clientMessageHeure);
-	                String heureResponse ="Heure:  "+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
-	                byte[] sendHeure= heureResponse.getBytes(); 
-	                // Convertit le message de réponse en tableau de bytes
-	                InetAddress clientAddress = receivePacketHeure.getAddress();
-	                // Obtient l'adresse IP du client
-	                int clientPort = receivePacketHeure.getPort();
-	                DatagramPacket sendPacket1 = new DatagramPacket(sendHeure, sendHeure.length, clientAddress, clientPort);
-	                // Crée un paquet pour envoyer la réponse au client
-	                serverSocket.send(sendPacket1); 
-	                // Envoie la réponse au client
+			 int serverPort = 1235; 
+	            DatagramSocket serverSocket = new DatagramSocket(serverPort); 
+	            // Crée une socket pour le serveur
 
+	            System.out.println("Le serveur est en attente de connexions..."); 
+
+	            while (true) { 
+	                byte[] receiveData = new byte[1024]; 
+	                // Crée un tableau de bytes pour stocker les données reçues
+	                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length); 
+	                // Crée un paquet pour recevoir des données
+	                serverSocket.receive(receivePacket); 
+	                // Attend la réception de données depuis un client
+	                String clientMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
+	                // Convertit les données reçues en une chaîne de caractères
+	                InetAddress clientAddress = receivePacket.getAddress();	  
+	                int clientPort = receivePacket.getPort(); 
+	                // Obtient le port du client et l'adresse IP du client
+	                
+	                // Réponse au client
+	                String welcomeMessage = "Bienvenu " + clientMessage;
+	                // Crée un message de réponse
+	                byte[] sendData = welcomeMessage.getBytes();
+	                // Convertit le message de réponse en tableau de bytes
+	                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, clientAddress, clientPort);
+	                // Crée un paquet pour envoyer la réponse au client
+	                serverSocket.send(sendPacket); 
+	                // Envoie la réponse au client
+	               
 		}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	 }
+	}
 
 }
